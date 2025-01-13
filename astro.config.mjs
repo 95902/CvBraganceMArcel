5722eb446a591164814753bd386d1227fe96eb89
95902 <Bragance.m@hotmail.fr>
2025-01-13 23:06:24 +0100

    Ajout de la configuration pour le déploiement sur GitHub Pages et mise à jour des chemins des feuilles de style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://CvBraganceMArcel.github.io",
+  // base:"CvBraganceMArcel",
+  trailingSlash:"always",
   integrations: [tailwind()],
 });
